571450bb054dac0bf842d8109634f8a08ff415ea
LazarDukov <lazarrumenowdukow@gmail.com>
2022-06-08 22:59:29 +0300

    tasks adding

---
+++ Java-Advanced-Exercises/FunctionalProgramming/e12t2KnightsOfHonour.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class e12t2KnightsOfHonour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> line = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        line.forEach(s -> System.out.println("Sir " + s));
+    }
+}
