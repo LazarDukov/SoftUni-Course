6c769fe7b20e8ec70937a4f6a66ac7036aef34e4
LazarDukov <lazarrumenowdukow@gmail.com>
2022-06-11 12:10:42 +0300

    Adding the solution of Company Roster problem

---
+++ Java-Advanced-Exercises/DefiningClasses/e14t2CompanyRoster/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, Department> personsList = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] inputData = scanner.nextLine().split("\\s+");
+            String name = inputData[0];
+            double salary = Double.parseDouble(inputData[1]);
+            String position = inputData[2];
+            String department = inputData[3];
+            Employee employee = null;
+            if (inputData.length == 4) {
+                employee = new Employee(name, salary, position, department);
+            } else if (inputData.length == 5 && inputData[4].contains("@")) {
+                String email = inputData[4];
+                int age = -1;
+                employee = new Employee(name, salary, position, department, email);
+            } else if (inputData.length == 5 && !inputData[4].contains("@")) {
+                int age = Integer.parseInt(inputData[4]);
+                employee = new Employee(name, salary, position, department, age);
+            } else if (inputData.length == 6) {
+                String email = inputData[4];
+                int age = Integer.parseInt(inputData[5]);
+                employee = new Employee(name, salary, position, department, email, age);
+            }
+            personsList.putIfAbsent(department, new Department(department));
+            personsList.get(department).getEmployees().add(employee);
+
+        }
+        Department highestDepartment = personsList.entrySet().stream()
+                .max(Comparator.comparing(e -> e.getValue().averageSalary()))
+                .get()
+                .getValue();
+
+        System.out.printf("Highest Average Salary: %s%n", highestDepartment.getName());
+        highestDepartment.getEmployees().stream()
+                .sorted((e1, e2) -> (int) e2.getSalary() - (int) e1.getSalary()).forEach(System.out::println);
+    }
+}
