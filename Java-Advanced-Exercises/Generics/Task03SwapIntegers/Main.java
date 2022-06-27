4282f0bb286cf3a990b7a6c4bcd5f1fde8c74963
LazarDukov <lazarrumenowdukow@gmail.com>
2022-06-27 20:44:53 +0300

    fourth task problem solved

---
+++ Java-Advanced-Exercises/Generics/Task03SwapIntegers/Main.java
@@ -0,0 +1,20 @@
+package generics;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box list = new Box();
+        for (int i = 0; i < n; i++) {
+            Integer input = Integer.parseInt(scanner.nextLine());
+            list.add(input);
+        }
+        String[] indeces = scanner.nextLine().split(" ");
+        int firstIndex = Integer.parseInt(indeces[0]);
+        int secondIndex = Integer.parseInt(indeces[1]);
+        list.swap(firstIndex, secondIndex);
+        System.out.println(list.toString());
+    }
+}
