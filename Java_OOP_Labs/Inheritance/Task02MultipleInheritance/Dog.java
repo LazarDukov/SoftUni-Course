a35f27d9aad71470d73913d8792381c36aea82fe
LazarDukov <lazarrumenowdukow@gmail.com>
2022-07-09 23:16:01 +0300

    added second task

---
+++ Java_OOP_Labs/Inheritance/Task02MultipleInheritance/Dog.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Dog extends Animal {
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
+//Animal with a single public method eat() that prints: "eating…"
+//Dog with a single public method bark() that prints: "barking…"
+//The Dog should inherit from the Animal.
---
+++ Java_OOP_Labs/Inheritance/Task02MultipleInheritance/Main.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}
