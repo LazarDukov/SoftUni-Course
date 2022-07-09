0875b31a46ab57a6433192659289ee0172964551
LazarDukov <lazarrumenowdukow@gmail.com>
2022-07-09 23:13:06 +0300

    Added first task

---
+++ Java_OOP_Labs/Inheritance/Task01SingleInheritance/Main.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
