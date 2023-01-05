7088e56e6ab3facb4e0bcd7623c96f5ab3f29d8f
Lazar Dukov <98097707+LazarDukov@users.noreply.github.com>
2023-01-05 21:37:02 +0200

    Add files via upload
    Import towns dto file created

--- Spring_Database-ExamPrep/PlayerFinder/src/main/java/com/example/football/models/dto/TownImportDto.java
+++ Spring_Database-ExamPrep/PlayerFinder/src/main/java/com/example/football/models/dto/TownImportDto.java
@@ -1,10 +1,53 @@
 package com.example.football.models.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 
 public class TownImportDto {
+    @Size(min = 2)
+    private String name;
 
-    
+    @Min(1)
+    private int population;
+
+
+    @Size(min = 10)
+    private String travelGuide;
+
+    public TownImportDto() {
+    }
+
+    public TownImportDto(String name, int population, String travelGuide) {
+        this.name = name;
+        this.population = population;
+        this.travelGuide = travelGuide;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getTravelGuide() {
+        return travelGuide;
+    }
+
+    public void setTravelGuide(String travelGuide) {
+        this.travelGuide = travelGuide;
+    }
 }
