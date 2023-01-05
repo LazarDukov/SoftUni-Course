a1ac07c10007cf8bb40797641555e3873c601cc1
Lazar Dukov <98097707+LazarDukov@users.noreply.github.com>
2023-01-05 21:39:52 +0200

    Add files via upload
    Town service updated and fill with data in overrided methods

--- Spring_Database-ExamPrep/PlayerFinder/src/main/java/com/example/football/service/impl/TownServiceImpl.java
+++ Spring_Database-ExamPrep/PlayerFinder/src/main/java/com/example/football/service/impl/TownServiceImpl.java
@@ -1,17 +1,24 @@
 package com.example.football.service.impl;
 
+import com.example.football.models.dto.TownImportDto;
+import com.example.football.models.entity.Town;
 import com.example.football.repository.TownRepository;
 import com.example.football.service.TownService;
 import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.xml.bind.JAXBContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import static com.example.football.util.Paths.TOWNS_PATH;
 
@@ -48,6 +55,24 @@ public class TownServiceImpl implements TownService {
         final String fileReader = readTownsFileContent();
         TownImportDto[] townImportDtos = this.gson.fromJson(fileReader, TownImportDto[].class);
 
-        return null;
+        List<String> result = new ArrayList<>();
+
+        for (TownImportDto townImportDto : townImportDtos) {
+            Set<ConstraintViolation<TownImportDto>> errors = this.validator.validate(townImportDto);
+            if (errors.isEmpty()) {
+                Optional<Town> isTownExist = this.townRepository.findTownByName(townImportDto.getName());
+                if (isTownExist.isEmpty()) {
+                    Town town = this.modelMapper.map(townImportDto, Town.class);
+                    this.townRepository.save(town);
+                    result.add(String.format("Successfully imported " + town.toString()));
+                } else {
+                    result.add("Invalid town");
+                }
+            } else {
+                result.add("Invalid town");
+            }
+
+        }
+        return String.join(System.lineSeparator(), result);
     }
 }
