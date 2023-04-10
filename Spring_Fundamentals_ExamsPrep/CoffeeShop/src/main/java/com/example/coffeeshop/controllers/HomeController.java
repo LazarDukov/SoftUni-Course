3d6d276080a12cbdbbde759e43b4f10f070b323c
LazarDukov <lazarrumenowdukow@gmail.com>
2023-04-10 23:40:59 +0300

    All pages created and add offer function was created

--- Spring_Fundamentals_ExamsPrep/CoffeeShop/src/main/java/com/example/coffeeshop/controllers/HomeController.java
+++ Spring_Fundamentals_ExamsPrep/CoffeeShop/src/main/java/com/example/coffeeshop/controllers/HomeController.java
@@ -1,12 +1,50 @@
 package com.example.coffeeshop.controllers;
 
+import com.example.coffeeshop.models.dtos.AddOfferDTO;
+import com.example.coffeeshop.services.AuthService;
+import com.example.coffeeshop.services.LoggedUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    private final LoggedUser loggedUser;
+    private final AuthService authService;
+
+    @Autowired
+    public HomeController(LoggedUser loggedUser, AuthService authService) {
+        this.loggedUser = loggedUser;
+        this.authService = authService;
+    }
+
+    @GetMapping("/index")
     private String index() {
+        if (this.authService.isUserLogged()) {
+            return "redirect:/home";
+        }
         return "index";
     }
+
+    @GetMapping("/home")
+    private String home(Model model) {
+        if (!this.authService.isUserLogged()) {
+            return "redirect:/";
+        }
+        model.addAttribute("")
+        return "home";
+    }
+
+    @GetMapping("/logout")
+    private String logout() {
+        this.loggedUser.logout();
+        return "index";
+    }
+
+
+
+
 }
--- Spring_Fundamentals_ExamsPrep/CoffeeShop/src/main/java/com/example/coffeeshop/repositories/CategoryRepository.java
+++ Spring_Fundamentals_ExamsPrep/CoffeeShop/src/main/java/com/example/coffeeshop/repositories/CategoryRepository.java
@@ -1,10 +1,13 @@
 package com.example.coffeeshop.repositories;
 
 import com.example.coffeeshop.models.entities.Category;
+import com.example.coffeeshop.models.enums.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    Category findByName(CategoryType categoryType);
 }
